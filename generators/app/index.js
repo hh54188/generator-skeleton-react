f0db084290113e969ec8b0541910c0d8239984a2
liguangyi <252438823@qq.com>
2018-07-15 01:01:19 +0800

    update

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,21 +4,28 @@ module.exports = class extends Generator {
   constructor(props, opts) {
     super(props, opts);
   }
-  sample() {
-    this.log("sample");
-  }
-  initializing() {
-    // Your initialization methods (checking current project state, getting configs, etc)
-    this.log("initializing");
-  }
+  // sample() {
+  //   this.log("sample");
+  // }
+  // initializing() {
+  //   // Your initialization methods (checking current project state, getting configs, etc)
+  //   this.log("initializing");
+  // }
   prompting() {
     // Where you prompt users for options (where you’d call this.prompt())
-    this.log("prompting");
     return this.prompt([
       {
         type: "input",
         name: "name",
         message: "Your project name:",
+        store: true
+      },
+      {
+        type: "list",
+        name: "tool",
+        message: "Choose dependencies install tool:",
+        default: "yarn",
+        choices: ["yarn", "npm"],
         store: true
       },
       {
@@ -33,28 +40,39 @@ module.exports = class extends Generator {
       this.destinationRoot(answers.name);
     });
   }
-  configuring() {
-    // Saving configurations and configure the project (creating .editorconfig files and other metadata files)
-    this.log("configuring");
-  }
-  default() {
-    // If the method name doesn’t match a priority, it will be pushed to this group.
-    this.log("default");
-  }
+  // configuring() {
+  //   // Saving configurations and configure the project (creating .editorconfig files and other metadata files)
+  //   this.log("configuring");
+  // }
+  // default() {
+  //   // If the method name doesn’t match a priority, it will be pushed to this group.
+  //   this.log("default");
+  // }
   writing() {
     // Where you write the generator specific files (routes, controllers, etc)
-    this.log("writing");
+    const pkgJson = {
+      devDependencies: {
+        eslint: "^3.15.0"
+      },
+      dependencies: {
+        react: "^16.2.0"
+      }
+    };
+
+    // Extend or create package.json file in destination path
+    this.fs.extendJSON(this.destinationPath("package.json"), pkgJson);
   }
-  conflicts() {
-    // Where conflicts are handled (used internally)
-    this.log("conflicts");
-  }
+  // conflicts() {
+  //   // Where conflicts are handled (used internally)
+  //   this.log("conflicts");
+  // }
   install() {
     // Where installations are run (npm, bower)
-    this.log("install");
+    // this.npmInstall();
+    this.yarnInstall();
   }
-  end() {
-    // Called last, cleanup, say good bye, etc
-    this.log("Goodbye");
-  }
+  // end() {
+  //   // Called last, cleanup, say good bye, etc
+  //   this.log("Goodbye");
+  // }
 };
